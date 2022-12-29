05b409251d9b0afb0c1e1a983c38ee76694e287b
NunArae <kilog9712@gmail.com>
2022-12-29 17:54:04 +0900

    거래소/경매장 프래그먼트 추가

--- app/src/main/java/com/uniq/lostmarket/MainActivity.kt
+++ app/src/main/java/com/uniq/lostmarket/MainActivity.kt
@@ -2,10 +2,34 @@ package com.uniq.lostmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragment_container_view_tag, MarketFragment())
+            .commit()
+
+        btn_market.setOnClickListener {
+            if (supportFragmentManager.findFragmentById(R.id.fragment_container_view_tag) !is MarketFragment) {
+                supportFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.fragment_container_view_tag, MarketFragment())
+                    .commit()
+            }
+        }
+
+        btn_auction.setOnClickListener {
+            if (supportFragmentManager.findFragmentById(R.id.fragment_container_view_tag) !is AuctionFragment) {
+                supportFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.fragment_container_view_tag, AuctionFragment())
+                    .commit()
+            }
+        }
     }
 }
